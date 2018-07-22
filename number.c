c58f141e61faecacb1e2ab0e764b20e109ac0da8
anjhanaks <41075462+anjhanaks@users.noreply.github.com>
2018-07-22 13:56:40 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number;
+scanf("%f",&number);
+if(number<0.0)
+ printf("negative");
+else if(number>0.0)
+ printf("positive");
+else
+ printf("zero");
+ retrun 0;
+ }
